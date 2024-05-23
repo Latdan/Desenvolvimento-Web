debc7dfbf81eb2a2c06ce5b32dbbc3be52940736
Latdan <latiman2014@gmail.com>
2024-05-23 13:16:43 -0300

    Enviando Lista 5

---
+++ Lista 5/html/js/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Por favor, digite o seu nome:");
+let idade = prompt("Por favor, digite a sua idade:");
+let emprego = prompt("Por favor, digite o seu emprego atual:");
+let endereco = prompt("Por favor, digite o seu endereço:");
+
+let mensagem = `Nome: ${nome}\nIdade: ${idade}\nEmprego Atual: ${emprego}\nEndereço: ${endereco}`;
+
+alert(mensagem);
---
+++ Lista 5/js/ex02.js
@@ -0,0 +1,8 @@
+const nome = "Gabriel Latiman";
+const idade = 21;
+const emprego = "Piloto de drift";
+const endereco = "Rua Jose Pessota n°249, São Paulo, SP";
+
+const mensagem = `Nome: ${nome}\nIdade: ${idade}\nEmprego Atual: ${emprego}\nEndereço: ${endereco}`;
+
+console.log(mensagem);
